4e4f6021a3b832c9a8ae456e318c8a996062d6ea
Mathias <mathimyh@ntnu.no>
2023-01-23 20:08:04 +0100

    Funker faktisk nå

--- main.jl
+++ main.jl
@@ -1,34 +1,33 @@
 using Pkg
 
+
 using Random
 using Plots
 
 plotlyjs()
 
-x = range(0,10, length=100)
-y = sin.(x)
-plot(x,y, label="sin(x)")
+function part1a(n::Int64)
+    times = []
+    # Run this as many times as you want, it will approach the actual probability distribution
+    for i in 1:n
+        pos = 0
+        # Use abs value as it does not matter if first step goes back or forward
+        pos += rand()
+        time = 1
+        # Count how many steps before it's over the line
+        while pos > 0 || time > 200
+            pos += rand(-1:1)
+            time += 1
+        end
+        push!(times, time)
+    end
+    histogram(times)
+    xlims!(0, 100)
+end
 
-# function part1a(n::Int64)
-#     pos = 0
-#     times = []
-#     # Run this as many times as you want, it will approach the actual probability distribution
-#     for i in 1:n
-#         time = 1
-#         # Use abs value as it does not matter if first step goes back or forward
-#         pos += (rand())
-#         while pos > 0
-#             pos += rand(-1:1)
-#             time += 1
-#         end
-#         push!(times, time)
-#     end
-#     histogram(times)
-# end
+function main()
+    part1a(1000)
+end
 
-# function main()
-#     part1a(10000)
-# end
-
-# main()
+main()
     
